472b946367848602bf556f026efe4b4ff3b56790
kyleathollfleming <kyleathollfleming@gmail.com>
2021-09-28 14:14:10 +0200

    More minor RepositoryConfig changes with dependancy changes to parent POM for database driver compatibility TODO: passing data from the domain to repository to the translator. passing data through flows (figure out what flows are)

--- Project 1/AccountSystemRepository/src/main/java/za/ac/nwu/ac/repo/config/RepositoryConfig.java
+++ Project 1/AccountSystemRepository/src/main/java/za/ac/nwu/ac/repo/config/RepositoryConfig.java
@@ -1,18 +1,10 @@
 package za.ac.nwu.ac.repo.config;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.orm.jpa.JpaTransactionManager;
-import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
-import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
-import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
-
-import java.util.Properties;
 
 @Configuration
 @EnableTransactionManagement
@@ -21,87 +13,5 @@ import java.util.Properties;
 @PropertySource(value = "classpath:application-db.properties")
 public class RepositoryConfig {
 
-
-    //todo you probably do not need any of the code inside this entire file
-    private static final String[] ENTITY_PACKAGES_TO_SCAN = {"za.ac.nwu.ac.domain.persistence"};
-    private static final String PERSISTENCE_UNIT_NAME = "account.system.persistence";
-
-    @Value("${spring.datasource.url}")
-    private String datasourceUrl;
-
-    @Value("${spring.datasource.username}")
-    private String username;
-
-    @Value("${spring.datasource.password}")
-    private String password;
-
-    @Bean
-    public LocalContainerEntityManagerFactoryBean entityManagerFactory(){
-        LocalContainerEntityManagerFactoryBean entityManagerFactoryBean = new LocalContainerEntityManagerFactoryBean();
-//        entityManagerFactoryBean.setDataSource(dataSource());
-        entityManagerFactoryBean.setPackagesToScan(ENTITY_PACKAGES_TO_SCAN);
-        entityManagerFactoryBean.setJpaVendorAdapter(new HibernateJpaVendorAdapter());
-        entityManagerFactoryBean.setJpaProperties(buildJpaProperties());
-        entityManagerFactoryBean.setPersistenceUnitName(PERSISTENCE_UNIT_NAME);
-
-        return entityManagerFactoryBean;
-    }
-
-    @Bean
-    public PlatformTransactionManager transactionManager(){
-        JpaTransactionManager transactionManager = new JpaTransactionManager();
-        transactionManager.setEntityManagerFactory(entityManagerFactory().getObject());
-        return transactionManager;
-
-    }/*
-    @Bean
-    public JdbcTemplate getJdbcTemplate(){
-        return new JdbcTemplate(dataSource());
-    }
-
-    @Bean
-    public DataSource dataSource(){
-        DriverManagerDataSource dataSource = new DriverManagerDataSource();
-        dataSource.setDriverClassName("com.microsoft.sqlserver");
-        try{
-            //TO DO figure out how to call MS SQL data source and other pieces of data
-
-
-            dataSource.setUser(username);
-            dataSource.setPassword(password);
-            dataSource.setURL(datasourceUrl);
-            dataSource.setImplicitCachingEnabled(true);
-            dataSource.setFastConnectionFailoverEnabled(true);
-
-            DataSource datasource = null;
-            return datasource;
-        }
-        catch (SQLException e){
-            throw new RuntimeException("Unable to connect to the DB", e);
-        }
-        return dataSource();
-    }*/
-
-    @Bean Properties buildJpaProperties(){
-        Properties properties = new Properties();
-        properties.setProperty("javax.persistence.transactionType", "jta");
-        properties.setProperty("hibernate.Integercode.use_reflection_optimizer", "true");
-        properties.setProperty("hibernate.transaction.factory_class", "org.hibernate.transaction.JTATransactionFactory");
-        properties.setProperty("hibernate.query.factory_class", "org.hibernate.hql.internal.classic.ClassicQueryTranslatorFactory");
-        properties.setProperty("hibernate.transaction.jta.platform", "org.hibernate.service.jta.platform.internal.SunOneJtaPlatform");
-
-        //TODO: find mssql dialect for hibernate
-        properties.setProperty("hibernate.dialect", "org.hibernate.dialect");
-        properties.setProperty("hibernate.generate_statistics", "false");
-        properties.setProperty("hibernate.use_sql)comments", "false");
-        properties.setProperty("hibernate.show_sql", "true");
-        properties.setProperty("hibernate.format_sql", "true");
-        properties.setProperty("hibernate.jdbc.batch_size", "500");
-        properties.setProperty("hibernate.order_inserts", "true");
-        properties.setProperty("hibernate.order_updates", "true");
-        properties.setProperty("hibernate.batch_versioned_data", "true");
-
-        return properties;
-    }
 
 }
