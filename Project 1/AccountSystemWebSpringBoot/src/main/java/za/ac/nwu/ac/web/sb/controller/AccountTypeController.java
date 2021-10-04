febcf8a3cd22708bb91f1d41c2f0b13d4d635438
kyleathollfleming <kyleathollfleming@gmail.com>
2021-10-04 11:55:18 +0200

    Fixed AccountTypeController, next issue is to solve accountTypeFlow in the translator layer which will enable me to continue with accountTransactionFlow and controllers.
    TODO: passing data from the domain to repository to the translator.
    passing data through flows (figure out what flows are)

--- Project 1/AccountSystemWebSpringBoot/src/main/java/za/ac/nwu/ac/web/sb/controller/AccountTypeController.java
+++ Project 1/AccountSystemWebSpringBoot/src/main/java/za/ac/nwu/ac/web/sb/controller/AccountTypeController.java
@@ -1,18 +1,35 @@
 package za.ac.nwu.ac.web.sb.controller;
 
 import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import za.ac.nwu.ac.domain.dto.AccountTypeDto;
 import za.ac.nwu.ac.domain.service.GeneralResponse;
+import za.ac.nwu.ac.logic.flow.CreateAccountTypeFlow;
+import za.ac.nwu.ac.logic.flow.FetchAccountTypeFlow;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("account-type")
 public class AccountTypeController {
+
+    private final FetchAccountTypeFlow fetchAccountTypeFlow;
+    private final CreateAccountTypeFlow createAccountTypeFlow;
+
+    @Autowired
+    public AccountTypeController(FetchAccountTypeFlow fetchAccountTypeFlow, @Qualifier("createAccountTypeFlowName") CreateAccountTypeFlow createAccountTypeFlow){
+        this.fetchAccountTypeFlow=fetchAccountTypeFlow;
+        this.createAccountTypeFlow=createAccountTypeFlow;
+    }
+
+    //This method fetches all account types from the account repository using the accountTypeDto implementation of accountTypeFlow
     @GetMapping("/all")
     @ApiOperation(value = "Gets all the configured Account types.", notes = "Returns a list of account types")
     @ApiResponses(value = {
@@ -21,25 +38,29 @@ public class AccountTypeController {
             @ApiResponse(code = 404, message = "Not Found", response = GeneralResponse.class),
             @ApiResponse(code = 500, message = "Internal Server Error", response = GeneralResponse.class)
     })
-    public ResponseEntity<GeneralResponse<String>> getAll()
+    public ResponseEntity<GeneralResponse<List<AccountTypeDto>>> getAll()
     {
-/*        TODO: figure out what flow is within certain areas
-        List<AccountTypeDto> accountTypes = fetchAccountTypeFlow.getAllAccountTypes();*/
-        GeneralResponse<String> response = new GeneralResponse<>(true, "No types found");
+//        TODO: figure out what flow is within certain areas
+        List<AccountTypeDto> accountTypes = fetchAccountTypeFlow.getAllAccountTypes();
+        GeneralResponse<List<AccountTypeDto>> response = new GeneralResponse<>(true, accountTypes);
         return new ResponseEntity<>(response, HttpStatus.OK );
     }
-/*
+
+    //Creates a new AccountType by using createAccountTypeFlow implementation in the translator layer
     @PostMapping("")
     @ApiOperation(value = "Creates a new AccountType", notes = "Creates a new AccountType in the DB")
     @ApiResponses(value = {
             @ApiResponse(code = 201, message = "The AccountType was created successfully", response = GeneralResponse.class),
             @ApiResponse(code = 400, message = "Bad Request", response = GeneralResponse.class),
             @ApiResponse(code = 500, message = "Internal Server Error", response = GeneralResponse.class)})
-    public ResponseEntity<GeneralResponse<AccountTypeDto>>(
+    public ResponseEntity<GeneralResponse<AccountTypeDto>> create(
         @ApiParam(value = "Request body to create a new AccountType.",
                 required = true)
-                @RequestBody AccountTypeDto accountType){
-
+                @RequestBody
+                AccountTypeDto accountType){
+        AccountTypeDto accountTypeResponse = createAccountTypeFlow.create(accountType);
+        GeneralResponse<AccountTypeDto> response = new GeneralResponse<>(true, accountTypeResponse);
+        return new ResponseEntity<>(response, HttpStatus.OK);
     }
 
 
@@ -52,19 +73,17 @@ public class AccountTypeController {
             @ApiResponse(code = 400, message = "Bad Request", response = GeneralResponse.class),
             @ApiResponse(code = 404, message = "Resource Not Found", response = GeneralResponse.class),
             @ApiResponse(code = 500, message = "Internal Server Error", response = GeneralResponse.class)})
-    public ResponseEntity<GeneralResponse<AccountTypeDto>>
-    {
-        Object mnemonic;
+    public ResponseEntity<GeneralResponse<AccountTypeDto>> getAccountType(
         @ApiParam(
-            value = "The mnemonic that uniquely identifies the AccountType.",
-            example ="MILES",
-            name = "mnemonic",
-            required = true)
+                value = "The mnemonic that uniquely identifies the AccountType.",
+                example ="MILES",
+                name = "mnemonic",
+                required = true)
+        @PathVariable("mnemonic") final String mnemonic){
 
-        @PathVariable("mmnemonic" final String mnemonic){
         AccountTypeDto accountType = fetchAccountTypeFlow.getAccountTypeByMnemonic(mnemonic);
         GeneralResponse<AccountTypeDto> response = new GeneralResponse<>(true, accountType);
-        return new ResponseEntity<>(response, HttpStatus.OK;
+        return new ResponseEntity<>(response, HttpStatus.OK);
 
 
 
@@ -72,5 +91,5 @@ public class AccountTypeController {
 
     }
 
-    }*/
-}
+
+
